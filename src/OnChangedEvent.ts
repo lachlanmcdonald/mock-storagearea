d4dc01123dcf84c8012120ff69768a184707c3b9
Lachlan McDonald <lachlanmcdonald@users.noreply.github.com>
2025-11-16 21:30:18 +0000

    RF

--- src/OnChangedEvent.ts
+++ src/OnChangedEvent.ts
@@ -9,35 +9,33 @@ import { StorageChangeCallback } from './Types';
 const EVENT_NAME = 'changed';
 
 export default function OnChangedEvent<H extends StorageChangeCallback>() {
-	const registered = new Map() as Map<H, (event: Event) => void>;
+	const callbackRegistration = new Map() as Map<H, (event: Event) => void>;
 	const eventTarget = new EventTarget();
 
 	/**
 	 * Registers an event listener callback to an event.
 	 */
 	function addListener(callback: H) {
-		const handleEvent = (event: Event) => {
+		const handler = (event: Event) => {
 			if (event instanceof CustomEvent) {
 				const { changes, areaName } = event.detail;
 				callback(changes, areaName);
 			}
 		};
 
-		eventTarget.addEventListener(EVENT_NAME, handleEvent);
-		registered.set(callback, handleEvent);
+		eventTarget.addEventListener(EVENT_NAME, handler);
+		callbackRegistration.set(callback, handler);
 	}
 
 	/**
 	 * Removes an event listener callback on an event.
 	 */
 	function removeListener(callback: H) {
-		const k = registered.has(callback);
+		if (callbackRegistration.has(callback)) {
+			const handler = callbackRegistration.get(callback)!;
 
-		if (k) {
-			const handleEvent = registered.get(callback) as (event: Event) => void;
-
-			eventTarget.removeEventListener(EVENT_NAME, handleEvent);
-			registered.delete(callback);
+			eventTarget.removeEventListener(EVENT_NAME, handler);
+			callbackRegistration.delete(callback);
 		}
 	}
 
@@ -45,19 +43,18 @@ export default function OnChangedEvent<H extends StorageChangeCallback>() {
 	 * Return if the event listener callback is registered on an event.
 	 */
 	function hasListener(callback: H) {
-		return registered.has(callback);
+		return callbackRegistration.has(callback);
 	}
 
 	/**
 	 * Return if any event listener callbacks are registered on an event.
 	 */
 	function hasListeners() {
-		return registered.size > 0;
+		return callbackRegistration.size > 0;
 	}
 
 	/**
-	 * Dispatches a new event which indicates a change has occurred within
-	 * a Storage Area.
+	 * Dispatches a new event which indicates a change has occurred within a Storage Area.
 	 */
 	function dispatch(changes: Record<string, chrome.storage.StorageChange>, areaName?: string) {
 		const event = new CustomEvent(EVENT_NAME, {
